7fa5ac7f044c520f5e2d6c38a6a1383140c92657
MrBreakIT <johnpike1022@gmail.com>
2020-09-11 10:36:57 -0500

    updatePokemonAPI

--- 03_MERN_Stack/Day7-APIs/pokemon-api/src/App.jsx
+++ 03_MERN_Stack/Day7-APIs/pokemon-api/src/App.jsx
@@ -6,19 +6,19 @@ function App() {
   const [names, setNames] = useState([]);
 
   const getNames = () => {
-    Axios.get("https://pokeapi.co/api/v2/pokemon")
-      .then(res => setNames(res.data.all))
-      .catch(err => console.log(err))
+    Axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
+      .then(response => setNames(response.data.results))
+      .catch(error => console.log(error))
   };
 
 
   return (
-    <div >
+    <div className=" d-flex flex-column mx-auto justify-content-center align-items-center p-3 m-5 ">
       <button className="btn btn-dark d-flex mx-auto p-3 m-5" onClick={getNames}>Fetch Pokemon</button>
       {
         names.map((poke, i) => {
-          return <p key={i}>{poke.text}</p>
-        })
+          return <p key={i}>{poke.name}</p>
+        }) 
       }
     </div>
   );
