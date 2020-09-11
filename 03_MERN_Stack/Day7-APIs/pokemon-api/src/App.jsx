7c6ae72664a7336592428580aa278ea1138f8a69
MrBreakIT <johnpike1022@gmail.com>
2020-09-11 15:56:12 -0500

    updatePokemon-api

--- 03_MERN_Stack/Day7-APIs/pokemon-api/src/App.jsx
+++ 03_MERN_Stack/Day7-APIs/pokemon-api/src/App.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Axios from 'axios';
+import {Router, Link} from '@reach/router';
 
 function App() {
   const [names, setNames] = useState([]);
+
+    // fetch version
+    // const getPokedata = () => {
+    //     fetch("https://pokeapi.co/api/v2/pokemon?offset=20&limit=1050")
+    //         .then((response) => response.json())
+    //         .then((response) => {
+    //             console.log(response.results);
+    //             setPokedata(response.results);
+    //         })
+    //         .catch((error) => console.log(error));
+    // };
 
   const getNames = () => {
     Axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
@@ -15,11 +27,12 @@ function App() {
   return (
     <div className=" d-flex flex-column mx-auto justify-content-center align-items-center p-3 m-5 ">
       <button className="btn btn-dark d-flex mx-auto p-3 m-5" onClick={getNames}>Fetch Pokemon</button>
+      <div className="d-flex  flex-wrap border border-info rounded m-4">
       {
         names.map((poke, i) => {
-          return <p key={i}>{poke.name}</p>
+          return <p className="col-3" key={i}>{poke.name}</p>
         }) 
-      }
+      }</div>
     </div>
   );
 }
