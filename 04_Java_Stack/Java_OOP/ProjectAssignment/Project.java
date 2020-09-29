af3f850d8763a7c577d2ceb1a235c1fba9e647d9
MrBreakIT <johnpike1022@gmail.com>
2020-09-29 16:29:36 -0500

    added cost variable

--- 04_Java_Stack/Java_OOP/ProjectAssignment/Project.java
+++ 04_Java_Stack/Java_OOP/ProjectAssignment/Project.java
@@ -1,6 +1,7 @@
 class Project {
     private String name;
     private String description;
+    private Double cost;
     
     // overload constructor method 3 ways
     public Project() {
@@ -12,6 +13,9 @@ class Project {
             this.name = name;
             this.description = description;
         }
+    public Project(Double cost){
+        this.cost = cost;
+        }
 
     //******************* getters *************
     public String getName(){
@@ -19,6 +23,9 @@ class Project {
     }
     public String getDescription(){
         return description;
+    }
+    public Double getCost(){
+        return cost;
     }
 
     //******************* setters *************
@@ -28,13 +35,16 @@ class Project {
     public void setDescription(String name){
         this.description = description;
     }
+    public void setCost(Double cost){
+        this.cost = cost;
+    }
 
     // ******************* this is instance method *************
     public void elevatorPitch(Project project){
-        System.out.println("Project Name: " + project.getName() + " Description: " + project.getDescription());
+        System.out.println("Project Name: " + project.getName() + " Description: " + project.getDescription() + ", Initial Cost: " + project.getCost());
     }
     // will run including String declaring what project number is running
     public void elevatorPitch(String str, Project project){
-        System.out.println(str + ": Project Name: " + project.getName() + " Description: " + project.getDescription());
+        System.out.println(str + ": Project Name: " + project.getName() + " Description: " + project.getDescription() + ", Initial Cost: " + project.getCost());
     }
 }
--- 04_Java_Stack/Java_OOP/ProjectAssignment/ProjectTest.java
+++ 04_Java_Stack/Java_OOP/ProjectAssignment/ProjectTest.java
@@ -1,6 +1,7 @@
 class ProjectTest {
     public static void main(String[] args) {
         Project p = new Project ();
+        
 
         Project project1 = new Project();
 
@@ -9,8 +10,10 @@ class ProjectTest {
 
         Project project3 = new Project("CheckCashing ,", "cash your checks now!");
         // System.out.println("Project name: " + project3.getName() + "Description: " + project3.getDescription());
+        project1.setCost(0.00);
+        project2.setCost(1000.00);
+        project3.setCost(100.00);
 
-
-        p.elevatorPitch("Project #3", project3);
+        p.elevatorPitch(project1);
     }
 }
